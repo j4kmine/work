1656ece4757a423590e360aaf0cabc8086e94153
bayu <bayu@katadata.co.id>
2020-07-07 19:37:52 +0700

    item

--- resources/views/cms/layouts/sidebar.blade.php
+++ resources/views/cms/layouts/sidebar.blade.php
@@ -32,6 +32,7 @@
                     </div>
                     <ul class="sidebar-menu">
                         <li class="treeview"><a href="<?php echo url('/order')?>"><i class="icon icon-document-list s-24"></i>Order</a></li>
+                        <li class="treeview"><a href="<?php echo url('/iten')?>"><i class="icon icon-queue s-24"></i>Item</a></li>
                         <li class="treeview"><a href="<?php echo url('/tracking')?>"><i class="icon-local_shipping s-24"></i> &nbsp;&nbsp;Tracking</a></li>
                         <li class="treeview"><a href="<?php echo url('/users')?>"><i class="icon icon-account_box s-24"></i>Users</a></li>
                         <li class="treeview"><a href="<?php echo url('/address')?>"><i class="icon icon-home2 s-24"></i>Alamat</a></li>
